289492b5e0860df4f0a35304962f2d2ddc460d55
CodeDotJS <rushforlinux@gmail.com>
2016-02-28 22:10:56 +0530

    mkdirp - inside func()

--- cli.js
+++ cli.js
@@ -8,36 +8,45 @@ const mkdirp = require('mkdirp');
 const md5 = require('md5');
 const colors = require('colors');
 const Parse = require('jsonparse');
+const boxen = require('boxen');
+const request = require('request');
 
 colors.setTheme({
-	directory: ['cyan', 'bold']
+    directory: ['cyan', 'bold']
 });
 
 colors.setTheme({
-	info: ['cyan', 'bold']
+    info: ['cyan', 'bold']
 });
 
 colors.setTheme({
-	normal: ['green', 'bold']
+    normal: ['green', 'bold']
 });
 
 const argv = require('yargs')
-	.usage('\nUsage : $0 -u [/email@id] -n [file name]'.info)
-	.demand(['u', 'n'])
-	.describe('u', 'Email-Id of any gravatar user')
-	.describe('n', 'Name of Image')
-	.argv;
+    .usage('\nUsage : $0 -u [/email@id] -n [file name]'.info)
+    .demand(['u', 'n'])
+    .describe('u', 'Email-Id of any gravatar user')
+    .describe('n', 'Name of Image')
+    .argv;
+
+const hashEmail = md5(argv.u);
 
 const saveImage = "./Gravatar/";
 
 const removeSlash = saveImage.replace("./", '');
 
-mkdirp(saveImage, function (err) {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log('\n\t ❭ Directory Created 	:'.directory + '    ✔'.normal);
-	}
+request('https://gravatar.com', function(error, response) {
+    if (!error && response.statusCode == 200) {
+    	mkdirp(saveImage, function(err) {
+    		if (err) {
+    			console.log(err);
+    		} else {
+    			console.log('\n\t ❭ Directory Created 	:'.directory + '    ✔'.normal);
+    		}
+    	});
+            console.log('\n\t ❭ Downloading 		:'.directory + '    ✔'.normal);
+    } else {
+        console.log(boxen(" SLOW INTERNET! ").rainbow);
+    }
 });
-
-const hashEmail = md5(argv.u);
