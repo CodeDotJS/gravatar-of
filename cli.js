dd81a60fe33d6f3a074a2afe66f610f3fa90201a
CodeDotJS <rushforlinux@gmail.com>
2016-02-29 20:52:21 +0530

    intrnet connection

--- cli.js
+++ cli.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
 'use strict';
-
+/* dependencies */
 const fs = require('fs');
-const http = require('follow-redirects').http;
+const http = require('follow-redirects').http; // just in case
 const mkdirp = require('mkdirp');
 const md5 = require('md5');
-const colors = require('colors');
 const Parse = require('jsonparse');
 const boxen = require('boxen');
 const request = require('request');
-
+const https = require('https');
+const colors = require('colors/safe');
 colors.setTheme({
     directory: ['cyan', 'bold']
 });
@@ -20,11 +20,15 @@ colors.setTheme({
 });
 
 colors.setTheme({
-    normal: ['green', 'bold']
+    normal: ['red', 'bold']
+});
+
+colors.setTheme({
+    connection: ['red', 'underline']
 });
 
 const argv = require('yargs')
-    .usage('\nUsage : $0 -u [/email@id] -n [file name]'.info)
+    .usage(colors.cyan.bold('\nUsage : $0 -u [/email@id] -n [file name]'))
     .demand(['u', 'n'])
     .describe('u', 'Email-Id of any gravatar user')
     .describe('n', 'Name of Image')
@@ -32,21 +36,16 @@ const argv = require('yargs')
 
 const hashEmail = md5(argv.u);
 
+request('https://google.com', function(error, response) {
+    if (!error && response.statusCode == 200) {
+        console.log('\n\t ❭ Internet Connection    :    '.directory + '✔'.normal);
+    } else {
+        console.log('\n');
+        console.log("              " + "Please check your internet connection".connection);
+        console.log('\n');
+    }
+});
+
 const saveImage = "./Gravatar/";
 
 const removeSlash = saveImage.replace("./", '');
-
-request('https://gravatar.com', function(error, response) {
-    if (!error && response.statusCode == 200) {
-    	mkdirp(saveImage, function(err) {
-    		if (err) {
-    			console.log(err);
-    		} else {
-    			console.log('\n\t ❭ Directory Created 	:'.directory + '    ✔'.normal);
-    		}
-    	});
-            console.log('\n\t ❭ Downloading 		:'.directory + '    ✔'.normal);
-    } else {
-        console.log(boxen(" SLOW INTERNET! ").rainbow);
-    }
-});
