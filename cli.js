a079ea5ccca06f4547fbcc2bf9f95e233f13e1e7
CodeDotJS <rushforlinux@gmail.com>
2016-03-02 01:40:54 +0530

    speed | dir

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 'use strict';
+
 /* dependencies */
+
 const fs = require('fs');
 const http = require('follow-redirects').http; // just in case
 const mkdirp = require('mkdirp');
@@ -10,7 +12,7 @@ const Parse = require('jsonparse');
 const boxen = require('boxen');
 const request = require('request');
 const https = require('https');
-const colors = require('colors/safe');
+const colors = require('colors');
 colors.setTheme({
     directory: ['cyan', 'bold']
 });
@@ -27,8 +29,12 @@ colors.setTheme({
     connection: ['red', 'underline']
 });
 
+colors.setTheme({
+	error: ['red', 'bold']
+});
+
 const argv = require('yargs')
-    .usage(colors.cyan.bold('\nUsage : $0 -u [/email@id] -n [file name]'))
+    .usage('\nUsage : $0 -u [/email@id] -n [file name]'.info)
     .demand(['u', 'n'])
     .describe('u', 'Email-Id of any gravatar user')
     .describe('n', 'Name of Image')
@@ -36,16 +42,26 @@ const argv = require('yargs')
 
 const hashEmail = md5(argv.u);
 
-request('https://google.com', function(error, response) {
-    if (!error && response.statusCode == 200) {
-        console.log('\n\t ❭ Internet Connection    :    '.directory + '✔'.normal);
-    } else {
-        console.log('\n');
-        console.log("              " + "Please check your internet connection".connection);
-        console.log('\n');
-    }
-});
+const usedAs = hashEmail;
 
 const saveImage = "./Gravatar/";
 
 const removeSlash = saveImage.replace("./", '');
+
+mkdirp(removeSlash, function(err) {
+    if (err) {
+        console.log(boxen("  Failed to create the directory   ").error);
+    } else {
+        console.log("\n\t ❭ Directory Created     :     ".directory + "✔");
+    }
+})
+
+request('http://rishigiri.com/gravatar/', function(error, response) {
+    if (!error && response.statusCode == 200) {
+        console.log('\n\t ❭ Internet Connection    :    '.directory + '✔'.normal);
+    } else {
+        console.log('\n');
+        console.log(boxen("  Please check your internet connection  ").error);
+        console.log('\n');
+    }
+});
