81b84f1fecd258232e6213bd133b9f552e8ebadb
CodeDotJS <rushforlinux@gmail.com>
2016-03-02 10:40:38 +0530

    filetype removed

--- filetype.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-
-request
-  .get('http://1.gravatar.com/avatar/343237491762eb627f25e0cd02527ac8')
-  .on('response', function(response) {
-    console.log(response.headers['content-type']) // 'image/png'
-  })
