31264d6a86b91c374390ee5dc266e2f0b4b73aec
CodeDotJS <rushforlinux@gmail.com>
2016-03-02 10:14:28 +0530

    remote filetype

--- cli.js
+++ cli.js
@@ -13,32 +13,35 @@ const boxen = require('boxen');
 const request = require('request');
 const https = require('https');
 const colors = require('colors');
+
 colors.setTheme({
-    directory: ['cyan', 'bold']
+  directory: ['cyan', 'bold']
 });
 
 colors.setTheme({
-    info: ['cyan', 'bold']
+  info: ['cyan', 'bold']
 });
 
 colors.setTheme({
-    normal: ['red', 'bold']
+  status: ['green', 'bold']
+})
+
+colors.setTheme({
+  normal: ['red', 'bold']
 });
 
 colors.setTheme({
-    connection: ['red', 'underline']
-});
-
-colors.setTheme({
-	error: ['red', 'bold']
+  error: ['red', 'bold']
 });
 
 const argv = require('yargs')
-    .usage('\nUsage : $0 -u [/email@id] -n [file name]'.info)
-    .demand(['u', 'n'])
-    .describe('u', 'Email-Id of any gravatar user')
-    .describe('n', 'Name of Image')
-    .argv;
+  .usage('\nUsage : $0 -u [/email@id] -n [file name]'.info)
+  .demand(['u', 'n'])
+  .describe('u', 'Email-Id of any gravatar user')
+  .describe('n', 'Name of Image')
+  .argv;
+
+const localFold = argv.n;
 
 const hashEmail = md5(argv.u);
 
@@ -48,22 +51,66 @@ const saveImage = "./Gravatar/";
 
 const removeSlash = saveImage.replace("./", '');
 
-mkdirp(removeSlash, function(err) {
-    if (err) {
-        console.log(boxen("  Failed to create the directory   ").error);
-    } else {
-        console.log("\n\t ❭ Directory Created     :     ".directory + "✔");
-    }
-})
+const forSaved = removeSlash.replace("/", '');
 
-request('http://rishigiri.com/gravatar/', function(error, response) {
-    if (!error && response.statusCode == 200) {
-        console.log('\n\t ❭ Internet Connection    :    '.directory + '✔'.normal);
-    } else {
-        console.log('\n');
-        console.log(boxen("  Please check your internet connection  ").error);
-        console.log('\n');
-    }
+mkdirp(removeSlash, function(err) {
+  if (err) {
+    console.log(boxen("  Failed to create the directory   ").error);
+  } else {
+    setTimeout(function() {
+      console.log("\n\t ❱ Directory Created      :    ".directory + "✔"
+        .status);
+    }, 1000);
+  }
 });
 
-const imageFile = fs.createWriteStream(removeSlash + argv.n + '.png');
+request('http://rishigiri.com/gravatar/', function(error, response) {
+  if (!error && response.statusCode == 200) {
+    console.log('\n\t ❱ Internet Connection    :    '.directory + "✔".status);
+  } else {
+    console.log('\n');
+    console.log(boxen("  Please check your internet connection  ").error);
+    console.log('\n');
+  }
+});
+
+
+request
+  .get('http://1.gravatar.com/avatar/' + usedAs)
+  .on('response', function(response) {
+    const storeType = response.headers['content-type'];
+    const parseType = storeType.toString().replace('image/', '');
+    const typeArray = ['png', 'jpeg', 'gif'];
+    if (typeArray[0] === parseType) {
+      const imageFile = fs.createWriteStream(removeSlash + argv.n + '.png\n');
+
+      http.get('http://1.gravatar.com/avatar/' + usedAs + '?size=400px',
+        function(res) {
+          res.pipe(imageFile);
+          setTimeout(function() {
+            console.log("\n\t ❱ Image Saved In         :    ".directory +
+              forSaved.toString().status + " ❱ " + localFold.toString()
+              .status +
+              "\n".status);
+          }, 2000);
+        }).on('error', function(err) {
+        console.log(err);
+      });
+    };
+    if (typeArray[1] === parseType) {
+      const imageFile = fs.createWriteStream(removeSlash + argv.n + '.jpeg\n');
+
+      http.get('http://1.gravatar.com/avatar/' + usedAs + '?size=400px',
+        function(res) {
+          res.pipe(imageFile);
+          setTimeout(function() {
+            console.log("\n\t ❱ Image Saved In         :    ".directory +
+              forSaved.toString().status + " ❱ " + localFold.toString()
+              .status +
+              "\n".status);
+          }, 2000);
+        }).on('error', function(err) {
+        console.log(err);
+      });
+    };
+  });
