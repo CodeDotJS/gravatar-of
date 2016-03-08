4dbdca0d5c53004d88f487510c1a47978b71c065
CodeDotJS <rushforlinux@gmail.com>
2016-03-08 19:01:13 +0530

    docs + mkdirp improved

--- cli.js
+++ cli.js
@@ -8,14 +8,10 @@ const fs = require('fs');
 const http = require('follow-redirects').http;
 const mkdirp = require('mkdirp');
 const md5 = require('md5');
-
-// const Parse = require('jsonparse');
-
-const boxen = require('boxen');
+const Parse = require('jsonparse');
 const request = require('request');
-
-// const cheerio = require('cheerio');
-
+const boxen = require('boxen');
+const cheerio = require('cheerio');
 const colors = require('colors');
 
 colors.setTheme({
@@ -39,24 +35,47 @@ colors.setTheme({
 });
 
 const argv = require('yargs')
-	.usage('\n Usage : $0 -u [/email@id] -n [file name]'.info)
+	.usage('\n Usage : $0 -u [email@id] -n [file name]'.info)
 	.demand(['u', 'n'])
-	.describe('u', '❱'.status + '  Email-Id of any gravatar user')
-	.describe('n', '❱'.status + '  Name of Image')
+	.describe('u', '❱'.status + ' Email-Id of any gravatar user')
+	.describe('n', '❱'.status + ' Name of Image')
 	.argv;
 
 const localFold = argv.n;
 
+// hashing and storing the email
 const hashEmail = md5(argv.u);
 
+// passed
 const usedAs = hashEmail;
 
+// defining directories
+// is the directory where the image will be saved
 const saveImage = './Gravatar/';
 
+// because I want to
 const removeSlash = saveImage.replace('./', '');
 
+// used where the directory is shown to the user
 const forSaved = removeSlash.replace('/', '');
 
+// finding username based on email address
+
+function removeString(emailAddress) {
+	// removing everything after '@', basic strategy used by gravtar for username
+	return emailAddress.replace(/\@.*/, '');
+}
+
+// stored argument
+const replacedString = removeString(argv.u);
+
+mkdirp(removeSlash, err => {
+	if (err) {
+		console.error(boxen('  Failed to create the directory   ').error);
+	} else { /* referred to another mkdirp */ }
+});
+
+// loading lightweight page to check internet connection and creating directory based on the same process.
 request('http://rishigiri.com/gravatar/', (error, response) => {
 	if (!error && response.statusCode === 200) {
 		console.log('\n\t ❱ Internet Connection    :    '.directory + '✔'.status);
@@ -70,23 +89,37 @@ request('http://rishigiri.com/gravatar/', (error, response) => {
 			}
 		});
 	} else {
+		// console when statusCode !== 200 or no internet connection
 		console.log('\n');
 		console.log(boxen('  ERROR : Please check your internet connection  ').error);
 		console.log('\n');
+		// stop the whole process
 		process.exit(1);
 	}
 });
+
+// requesting for image with hashed email address which was previously stored in 'usedAs'
 
 request
 	.get('http://1.gravatar.com/avatar/' + usedAs)
 
 .on('response', response => {
+	// checking for the type of remote image.
 	const storeType = response.headers['content-type'];
+
+	// initially the result comes like image/.gif
+	// thus removing the 'image/' part
 	const parseType = storeType.toString().replace('image/', '');
+
+	// stored the possible extension in typeArray
 	const typeArray = ['png', 'jpeg', 'gif'];
 
-	if (typeArray[0] === parseType) {
+	// checking if the remote image is png
+	if (response.statusCode === 200 && typeArray[0] === parseType) {
+		// will save the image in the default defined directory
 		const imageFile = fs.createWriteStream(removeSlash + argv.n + '.png');
+
+		// started downloading image
 		http.get('http://1.gravatar.com/avatar/' + usedAs + '?size=400px',
 			res => {
 				res.pipe(imageFile);
@@ -98,9 +131,11 @@ request
 			}).on('error', err => {
 				console.log(err);
 			});
+	} else {
+		/* something to be done */
 	}
-
-	if (typeArray[1] === parseType) {
+	// checking if the remote image if jpg/jpeg
+	if (response.statusCode === 200 && typeArray[1] === parseType) {
 		const imageFile = fs.createWriteStream(removeSlash + argv.n + '.jpeg');
 
 		http.get('http://1.gravatar.com/avatar/' + usedAs + '?size=400px',
@@ -114,13 +149,7 @@ request
 			}).on('error', err => {
 				console.log(err);
 			});
+	} else {
+		/* do something */
 	}
 });
-
-function removeString(emailAddress) {
-	return emailAddress.replace(/\@.*/, '');
-}
-
-const replacedString = removeString(argv.u);
-
-console.log(boxen(' USERNAME : ' + replacedString.toLowerCase() + ' ').status);
